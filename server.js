7f9091ce3f0e56eb6109e5003ce9e1a9e34c12b2
wcbridge <33330345+wcbridge@users.noreply.github.com>
2018-05-18 17:37:03 -0400

    goingok

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ var PORT = 3500;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, "/app/public/index.html")));
+
+
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+    console.log("The times they are a changin")
+});
